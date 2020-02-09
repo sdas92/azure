c0c94c13b8552e3275d6293c60436f62d90d89e4
sdas92 <somnath-das@outlook.com>
2020-02-09 15:26:54 +0530

    Added some azure powershell scripts

---
+++ azPowershell/subscription-csv.ps1
@@ -0,0 +1,16 @@
+az login
+$subscriptions = az account list --all | ConvertFrom-Json
+
+$outputPath = "\\Somnath-Das\e$\logs\az.csv"
+$allsubList = @()
+
+foreach($sub in $subscriptions) {
+  $objSubs = New-Object psobject
+  $objSubs | Add-Member -MemberType NoteProperty -Name "SubscriptionID" -Value $sub.id
+  $objSubs | Add-Member -MemberType NoteProperty -Name "SubscriptionName" -Value $sub.name
+  $objSubs | Add-Member -MemberType NoteProperty -Name "Check" -Value "Y"
+  $allsubList += $objSubs
+  $objSubs = $null
+}
+
+$allsubList | Export-Csv -Path  $outputPath -NoTypeInformation
---
+++ azPowershell/tagAllUnderSubscription.ps1
@@ -0,0 +1,35 @@
+az login
+
+#Global variables
+$subscriptionID = "Your_subscription_ID"
+$tagApp = "AppName"
+$tagCountry = "India"
+
+
+#Tag the resource groups.
+Function tagResourceGroups
+{
+  param([string]$RG)
+  az group update --resource-group $RG --tags Application=$tagApp Country=$tagCountry
+}
+
+#Tag all resources under a RG.
+Function tagResources
+{
+  param([string]$Resource)
+  az resource tag --tags Application=$tagApp Country=$tagCountry --ids $Resource
+}
+
+#Get all resource groups and tag everything..
+$allresourceGroups = az group list --subscription $subscriptionID | ConvertFrom-Json
+foreach($resourceGroup in $allresourceGroups)
+{
+  $rgName = $resourceGroup.name
+  tagResourceGroups -RG $rgName
+  $allResources = az resource list --resource-group $rgName | ConvertFrom-Json
+  foreach($resource in $allResources)
+  {
+    $resourceID = $resource.id
+    tagResources -Resource $resourceID
+  }
+}
