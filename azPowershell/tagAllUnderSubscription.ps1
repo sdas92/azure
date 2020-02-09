ea3394e3baa2967420ae90113290b61941b80f1b
sdas92 <somnath-das@outlook.com>
2020-02-09 16:20:42 +0530

    Updated resource tagging script

--- azPowershell/tagAllUnderSubscription.ps1
+++ azPowershell/tagAllUnderSubscription.ps1
@@ -4,22 +4,27 @@ az login
 
 #Global variables
 $subscriptionID = "Your_subscription_ID"
-$tagApp = "AppName"
+$tagApp = "Learning App"
+$tagBrand = "DasLearning"
+$tagDivision = "IT"
 $tagCountry = "India"
+$tagRegion = "APAC"
+$tagEnvironment = "DEV"
+$tagService = "Monitoring"
 
 
 #Tag all resource groups.
 Function tagResourceGroups
 {
   param([string]$RG)
-  az group update --resource-group $RG --tags Application=$tagApp Country=$tagCountry
+  az group update --resource-group $RG --tags Application=$tagApp Brand=$tagBrand Division=$tagDivision Country=$tagCountry Region=$tagRegion Environment=$tagEnvironment Service=$tagService
 }
 
 #Tag all resources under a RG.
 Function tagResources
 {
   param([string]$Resource)
-  az resource tag --tags Application=$tagApp Country=$tagCountry --ids $Resource
+  az resource tag --ids $Resource --tags Application=$tagApp Brand=$tagBrand Division=$tagDivision Country=$tagCountry Region=$tagRegion Environment=$tagEnvironment Service=$tagService
 }
 
 #Get all resource groups and tag everything..
